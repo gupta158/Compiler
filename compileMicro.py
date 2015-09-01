ddde1abb9acbcded86d29b38d948f92148bbda7c
Manish Gupta <gupta158@purdue.edu>
2015-09-01 01:08:03 -0400

    Added bash script and test code pulled from git for antlr

---
+++ compileMicro.py
@@ -0,0 +1,21 @@
+__author__ = 'gupta158, jalliger'
+
+import sys
+from antlr4 import *
+from antlr4.InputStream import InputStream
+from MicroExprLexer import MicroExprLexer
+from MicroExprParser import MicroExprParser
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        input_stream = FileStream(sys.argv[1])
+    else:
+        input_stream = InputStream(sys.stdin.readline())
+
+    lexer = MicroExprLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = MicroExprParser(token_stream)
+    tree = parser.prog()
+
+    lisp_tree_str = tree.toStringTree(recog=parser)
+    print(lisp_tree_str)
